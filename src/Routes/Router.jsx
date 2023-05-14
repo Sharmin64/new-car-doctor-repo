015a0a7affb02b592b98188969a035d4db39fbe8
Sharmin64 <lizaeatsbanana@gmail.com>
2023-05-14 18:49:10 +0600

    auth provider set up done

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
-import About from "../Pages/Home/About/About";
 import Login from "../Pages/Login/Login";
+import SignUp from "../Pages/Signup/Signup";
 
 const router = createBrowserRouter([
   {
@@ -16,6 +16,10 @@ const router = createBrowserRouter([
       {
         path: "/login",
         element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <SignUp />,
       },
     ],
   },
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import "./index.css";
 import router from "./Routes/Router";
 import { RouterProvider } from "react-router-dom";
+import AuthProvider from "./Providers/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div className="max-w-7xl mx-auto">
     <React.StrictMode>
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </React.StrictMode>
     ,
   </div>
